e5b719159093c3247de4d82ae2c46d4efb08ee60
Valeria Agrest <agrestval@gmail.com>
2022-05-19 21:57:09 +0300

    jenkins

---
+++ tests_jenkins/test_j.py
@@ -0,0 +1,38 @@
+import allure
+from selenium import webdriver
+import unittest
+
+
+class GoogleTest(unittest.TestCase):
+    browser = None
+    google_title = "Google"
+    google_url = "https://www.google.com/"
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.browser = webdriver.Chrome()
+        cls.browser.maximize_window()
+        cls.browser.get(cls.google_url)
+        cls.browser.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.browser.quit()
+
+    @allure.story("Check Google's url")
+    def test_url(self):
+        with allure.step("Define current url"):
+            current_url = self.browser.current_url
+        with allure.step("Assert current url equals Google's url"):
+            self.assertEqual(current_url, self.google_url)
+
+    @allure.story("Check title of the Google's site")
+    def test_title(self):
+        with allure.step("Define url's title"):
+            title = self.browser.title
+        with allure.step("Assert current title equals Google's title"):
+            self.assertEqual(title, self.google_title)
+
+
+if __name__ == "__main__":
+    unittest.main()
