0f649b4a919ac8e58a51a18e34f4569ff1654af8
Valeria Agrest <agrestval@gmail.com>
2022-05-20 16:09:01 +0300

    test20

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope='class')
 def driver():
-    s = Service()
+    s = Service("usr/bin/chromedriver")
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
