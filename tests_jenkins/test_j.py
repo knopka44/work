987317701fc12b59f82c6ab1d63a02d9bd183131
Valeria Agrest <agrestval@gmail.com>
2022-05-20 14:11:41 +0300

    test3

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -5,10 +5,12 @@ from test_j_info import *
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+import os
 
 
 @pytest.fixture(scope='class')
 def driver():
+    os.chmod("/home/valerya/PycharmProjects/My_homework/tests/chromedriver", 755)
     s = Service("/home/valerya/PycharmProjects/My_homework/tests/chromedriver")
     driver = webdriver.Chrome(service=s)
     driver.maximize_window()
