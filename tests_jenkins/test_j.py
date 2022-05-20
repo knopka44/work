ecf01ebec4e36ca7d074abfd5e17d08c7278c923
Valeria Agrest <agrestval@gmail.com>
2022-05-20 19:50:53 +0300

    Revert "Squashed commit of the following:"
    This reverts commit bbd08cfea8e10400657612d6f457a3b87979d4a1.

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -8,12 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture(scope='class')
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--window-size=1420,1080')
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome("/home/valerya/PycharmProjects/My_homework/tests/chromedriver")
     driver.maximize_window()
     driver.get(url)
     driver.implicitly_wait(10)
