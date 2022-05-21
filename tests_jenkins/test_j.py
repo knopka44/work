fc42270e6ac2592a0cb13c72005a2ee01e1bc5a0
Valeria Agrest <agrestval@gmail.com>
2022-05-21 11:33:19 +0300

    fix chrome launch return some options v.2

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -11,6 +11,7 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--window-size=1420,1080')
+    options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(chrome_options=options)
     driver.maximize_window()
