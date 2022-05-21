08553c33b76f4fcfb3ee223f20f9292a440e50a4
Valeria Agrest <agrestval@gmail.com>
2022-05-21 14:02:11 +0300

    fix chrome launch with new options8

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -1,23 +1,21 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from test_j_info import *
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='class')
 def driver():
     options = Options()
-    # options.add_argument("--headless")  # Runs Chrome in headless mode.
+    options.add_argument("--headless")  # Runs Chrome in headless mode.
     options.add_argument('--no-sandbox')  # # Bypass OS security model
-    # options.add_argument("--disable-dev-shm-using")
     options.add_argument('--remote-debugging-port=9222')
-
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(options=options)
 
     driver.maximize_window()
     driver.get(url)
