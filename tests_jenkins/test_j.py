192e585f92b501dba4bee93081a9ad4b30e7364c
Valeria Agrest <agrestval@gmail.com>
2022-05-21 12:42:36 +0300

    fix chrome launch with new options4

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -4,16 +4,21 @@ from selenium.webdriver.common.by import By
 from test_j_info import *
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='class')
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--window-size=1420,1080')
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(options=options)
+    webdriver_path = '/usr/bin/chromedriver'
+    options = Options()
+    options.add_argument("--headless")  # Runs Chrome in headless mode.
+    options.add_argument('--no-sandbox')  # # Bypass OS security model
+    options.add_argument('start-maximized')
+    options.add_argument('disable-infobars')
+    options.add_argument("--disable-extensions")
+    driver = webdriver.Chrome(chrome_options=options,
+                              executable_path=webdriver_path)
+
     driver.maximize_window()
     driver.get(url)
     driver.implicitly_wait(10)
