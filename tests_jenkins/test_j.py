fa9ce1c80607d953733fc4cd6a95f3f6c662b17a
Valeria Agrest <agrestval@gmail.com>
2022-05-22 21:47:28 +0300

    pytest with chrome 42

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -19,7 +19,7 @@ def driver():
     # driver_options.add_argument("--disable-dev-shm-usage")
     driver_options.binary_location = '/usr/bin/google-chrome'
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=driver_options)
+    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=driver_options)
     driver.maximize_window()
     driver.get(url)
     driver.implicitly_wait(10)
