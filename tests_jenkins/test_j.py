01b75f1acee89412471958e8a9a56085f4a944c2
Valeria Agrest <agrestval@gmail.com>
2022-05-22 20:00:05 +0300

    pytest with chrome 14

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -14,7 +14,7 @@ def driver():
     driver_options.add_argument('--headless')
     driver_options.add_argument('--no-sandbox')
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    driver = webdriver.Chrome(executable_path='home/valerya/Programming/chromedriver',
+    driver = webdriver.Chrome(executable_path='/home/valerya/PycharmProjects/My_homework/tests/chromedriver',
                               options=driver_options)
     driver.maximize_window()
     driver.get(url)
