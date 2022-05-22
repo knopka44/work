b3b94c1a94556206c2542cf72fa83c573cc53b5f
Valeria Agrest <agrestval@gmail.com>
2022-05-22 18:40:43 +0300

    unittest

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -1,44 +1,59 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from test_j_info import *
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
+import unittest
+from test_j_info import Calculator
 
 
-@pytest.fixture(scope='class')
-def driver():
-    s = Service('chromedriver')
-    driver = webdriver.Chrome(service=s)
-    driver.maximize_window()
-    driver.get(url)
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
+class TestCalculator(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.calc = Calculator()
+
+    @classmethod
+    def tearDownClass(cls):
+        print('This is a unittest for calculator')
+
+    def test_sum(self):
+        answer = self.calc.sum(15, 4)
+        self.assertEqual(answer, 19)
+
+    def test_sub(self):
+        answer = self.calc.sub(26, 6)
+        self.assertEqual(answer, 20)
+
+    def test_mult(self):
+        answer = self.calc.mult(7, 3)
+        self.assertEqual(answer, 21)
+
+    def test_div(self):
+        answer = self.calc.div(22, 11)
+        self.assertEqual(answer, 2)
+
+    def test_sum_not_eq(self):
+        answer = self.calc.sum(6, 10)
+        self.assertNotEqual(answer, 15)
+
+    def test_sub_not_eq(self):
+        answer = self.calc.sub(33, 3)
+        self.assertNotEqual(answer, 36)
+
+    def test_mult_not_eq(self):
+        answer = self.calc.mult(8, 8)
+        self.assertNotEqual(answer, 60)
+
+    def test_div_not_eq(self):
+        answer = self.calc.div(55, 11)
+        self.assertNotEqual(answer, 7)
+
+    def test_div_y_zero(self):
+        with self.assertRaises(AssertionError) as er:
+            self.calc.div(7, 0)
+        self.assertEqual('y must not be 0', er.exception.args[0])
+
+    def test_validation(self):
+        with self.assertRaises(AssertionError) as er:
+            self.calc.validation(15, '6')
+        self.assertEqual('x or y must be int', er.exception.args[0])
 
 
-class TestDynamicControls:
-
-    def test_checkbox_is_gone(self, driver):
-        checkbox = driver.find_element(by=By.XPATH, value=f'{checkbox_site}')
-        checkbox.click()
-        remove = driver.find_element(by=By.XPATH, value=f'{remove_site}')
-        remove.click()
-        message = driver.find_element(by=By.ID, value=f'{message_site}')
-        WebDriverWait(driver, 15).until(EC.visibility_of(message))
-        invisibility = WebDriverWait(driver, 15).until(
-            EC.invisibility_of_element(checkbox))
-        assert invisibility is True
-
-    def test_input_disabled(self, driver):
-        input_text = driver.find_element(by=By.XPATH, value=f'{input_site}')
-        assert input_text.get_attribute('disabled') == 'true'
-
-    def test_input_enabled(self, driver):
-        enable = driver.find_element(by=By.XPATH, value=f'{enable_site}')
-        enable.click()
-        message = driver.find_element(by=By.ID, value=f'{message_site}')
-        WebDriverWait(driver, 15).until(EC.visibility_of(message))
-        input_text = driver.find_element(by=By.XPATH, value=f'{input_site}')
-        assert input_text.get_attribute('disabled') is None
+if __name__ == "__main__":
+    unittest.main()
--- tests_jenkins/test_j_info.py
+++ tests_jenkins/test_j_info.py
@@ -1,6 +1,28 @@
-url = 'http://the-internet.herokuapp.com/dynamic_controls'
-checkbox_site = '//input[@type="checkbox"]'
-remove_site = '//*[@id="checkbox-example"]/button'
-message_site = 'message'
-input_site = '//input[@type="text"]'
-enable_site = '//*[@id ="input-example"]/button'
+class Calculator:
+
+    @staticmethod
+    def validation(x, y):
+        assert all(isinstance(number, int) for number in (x, y)), \
+            "x or y must be int"
+        return True
+
+    def sum(self, x, y):
+        self.validation(x, y)
+        result = x + y
+        return result
+
+    def sub(self, x, y):
+        self.validation(x, y)
+        result = x - y
+        return result
+
+    def mult(self, x, y):
+        self.validation(x, y)
+        result = x * y
+        return result
+
+    def div(self, x, y):
+        assert y != 0, 'y must not be 0'
+        self.validation(x, y)
+        result = x / y
+        return result
