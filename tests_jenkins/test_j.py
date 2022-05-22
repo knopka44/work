9f448d6de5bd488b4d0e19ac777437b922da7e9e
Valeria Agrest <agrestval@gmail.com>
2022-05-22 21:21:01 +0300

    pytest with chrome 39

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -17,7 +17,7 @@ def driver():
     # driver_options.add_argument("--disable-extensions")
     # driver_options.add_argument("--disable-gpu")
     # driver_options.add_argument("--disable-dev-shm-usage")
-    driver_options.binary_location = '/usr/bin/google-chrome'
+    driver_options.binary_location = '/usr/bin/'
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     driver = webdriver.Chrome(options=driver_options)
     driver.maximize_window()
