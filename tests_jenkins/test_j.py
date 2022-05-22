96b12ce9742e1f1eee32950dc938312a98da2cb1
Valeria Agrest <agrestval@gmail.com>
2022-05-22 22:01:45 +0300

    pytest with chrome 45

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -16,8 +16,8 @@ def driver():
     driver_options.add_argument('--user-data-dir=~/.config/google-chrome')
     # driver_options.add_argument('disable-infobars')
     # driver_options.add_argument("--disable-extensions")
-    # driver_options.add_argument("--disable-gpu")
-    # driver_options.add_argument("--disable-dev-shm-usage")
+    driver_options.add_argument("--disable-gpu")
+    driver_options.add_argument("--disable-dev-shm-usage")
     # driver_options.binary_location = '/usr/bin/google-chrome'
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     driver = webdriver.Chrome(options=driver_options)
