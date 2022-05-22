a231bf4135280a07946c29ba88f48b6b29f68a61
Valeria Agrest <agrestval@gmail.com>
2022-05-22 20:36:58 +0300

    pytest with chrome 28

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -17,7 +17,7 @@ def driver():
     driver_options.add_argument("--disable-extensions")
     driver_options.add_argument("--disable-gpu")
     driver_options.add_argument("--disable-dev-shm-usage")
-    driver_options.add_argument('--user-data-dir=chromedriver')
+    driver_options.add_argument('--user-data-dir=https://github.com/agrestvaleria/homework/chromedriver')
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     driver = webdriver.Chrome(options=driver_options)
     driver.maximize_window()
