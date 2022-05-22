bc42054343af648515bb5b6bd88976242877f3f0
Valeria Agrest <agrestval@gmail.com>
2022-05-23 00:06:32 +0300

    pytest with chrome 59

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -5,17 +5,18 @@ from test_j_info import *
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import warnings
+import os
 
 
 @pytest.fixture(scope='class')
 def driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--window-size=1420,1080')
+    os.environ['DISPLAY'] = ':20.0'
+    options.add_argument('--no-sandbox')
+    options.add_argument('--window-size=1420,1080')
     options.add_argument('--headless')
-    options.add_argument('--user-data-dir=/home/valerya/.config/google-chrome/Profile1')
-    # options.add_argument('--disable-gpu')
-    # options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     driver.get(url)
