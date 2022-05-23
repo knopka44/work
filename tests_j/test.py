709bca7692d508016bb0fd663a24e05ccc5a9abb
Valeria Agrest <agrestval@gmail.com>
2022-05-23 12:05:00 +0300

    service args 7

--- tests_j/test.py
+++ tests_j/test.py
@@ -19,8 +19,8 @@ def web_driver():
     # options.add_argument('--headless')
     # options.binary_location = "/usr/bin/chromium-browser"
     #
-    options = Options()
-    options.binary_location = '/usr/bin/chromium-browser'
+    options = webdriver.ChromeOptions()
+    # options.binary_location = '/usr/bin/chromium-browser'
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-gpu")
@@ -33,7 +33,7 @@ def web_driver():
     # options.add_argument(f"--data-path={mkdtemp()}")
     # options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome(service=Service('/snap/bin/chromium.chromedriver'),
+    driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'),
                               options=options, service_args=["--verbose", "--log-path=/home/valerya/PycharmProjects/qc1.log"])
 
     # driver = webdriver.Chrome( options=options, service_args=["--verbose", "--log-path=/home/valerya/PycharmProjects/qc1.log"])
