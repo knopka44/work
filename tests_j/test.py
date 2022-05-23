8cc96b7aad2be330e215be7bf47b7683fa16cb43
Valeria Agrest <agrestval@gmail.com>
2022-05-23 09:41:17 +0300

    pytest with chrome 69

--- tests_j/test.py
+++ tests_j/test.py
@@ -6,9 +6,10 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='session')
 def web_driver():
     options = Options()
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
     driver.maximize_window()
     driver.get("https://www.google.com/")
     driver.implicitly_wait(10)
