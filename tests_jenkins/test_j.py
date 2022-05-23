167ddeb80ddec477f2bd0a609af4d2a3278c89d6
knopka44 <103408196+knopka44@users.noreply.github.com>
2022-05-23 18:14:33 +0300

    Update test_j.py

--- tests_jenkins/test_j.py
+++ tests_jenkins/test_j.py
@@ -1,51 +1,40 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.service import Service
 
 from test_j_info import *
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import warnings
+from dynamic_controls_page import *
 
 
 @pytest.fixture(scope='class')
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=chrome_options)
     browser.set_window_size(1920, 1080)
     
-    driver.get(url)
+    browser.get(url)
     browser.implicitly_wait(2)
-    yield driver
-    driver.quit()
+    yield browser
+    browser.quit()
 
 
-class TestDynamicControls:
+def test_remove_checkbox(driver):
+    page = DynamicControlsPage(driver)
+    page.remove_checkbox()
+    assert page.is_checkbox_present() == 0
 
-    def test_checkbox_is_gone(self, driver):
-        checkbox = driver.find_element(by=By.XPATH, value=f'{checkbox_site}')
-        checkbox.click()
-        remove = driver.find_element(by=By.XPATH, value=f'{remove_site}')
-        remove.click()
-        message = driver.find_element(by=By.ID, value=f'{message_site}')
-        WebDriverWait(driver, 15).until(EC.visibility_of(message))
-        invisibility = WebDriverWait(driver, 15).until(
-            EC.invisibility_of_element(checkbox))
-        assert invisibility is True
 
-    def test_input_disabled(self, driver):
-        input_text = driver.find_element(by=By.XPATH, value=f'{input_site}')
-        assert input_text.get_attribute('disabled') == 'true'
+def test_disabled_input(driver):
+    page = DynamicControlsPage(driver)
+    assert page.is_element_disabled() is True
 
-    def test_input_enabled(self, driver):
-        enable = driver.find_element(by=By.XPATH, value=f'{enable_site}')
-        enable.click()
-        message = driver.find_element(by=By.ID, value=f'{message_site}')
-        WebDriverWait(driver, 15).until(EC.visibility_of(message))
-        input_text = driver.find_element(by=By.XPATH, value=f'{input_site}')
-        assert input_text.get_attribute('disabled') is None
+
+def test_enabled_input(driver):
+    page = DynamicControlsPage(driver)
+    page.click_on_enable_button()
+    assert page.is_element_disabled() is False
