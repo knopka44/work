8a9c89762aa508001a8e4300bb9ed6d2d512474f
Valeria Agrest <agrestval@gmail.com>
2022-05-23 09:37:33 +0300

    pytest with chrome 68

--- tests_j/test.py
+++ tests_j/test.py
@@ -1,16 +1,13 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='session')
 def web_driver():
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--no-sandbox')
-    options.add_argument('--window-size=1420,1080')
-    options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
-    # options.add_argument('--remote-debugging-port=9222')
-    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.get("https://www.google.com/")
