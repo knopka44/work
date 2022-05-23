5dda6035ef207c41894b5a373395209cedfcc1f1
Valeria Agrest <agrestval@gmail.com>
2022-05-23 13:28:49 +0300

    service args after reinstall chrome & webdriver 1

--- tests_j/test.py
+++ tests_j/test.py
@@ -32,8 +32,7 @@ def web_driver():
     # options.add_argument(f"--data-path={mkdtemp()}")
     # options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     # options.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome(options=options)
-
+    driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
     # driver = webdriver.Chrome( options=options, service_args=["--verbose", "--log-path=/home/valerya/PycharmProjects/qc1.log"])
     driver.maximize_window()
     driver.get("https://www.google.com/")
