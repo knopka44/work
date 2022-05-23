3b8920fe39ee6f87f79909b931c531763ab23ce4
Valeria Agrest <agrestval@gmail.com>
2022-05-23 11:42:43 +0300

    service args 4

--- tests_j/test.py
+++ tests_j/test.py
@@ -24,16 +24,16 @@ def web_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280x1696")
+    # options.add_argument("--window-size=1280x1696")
     options.add_argument("--single-process")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-dev-tools")
     options.add_argument("--no-zygote")
-    options.add_argument(f"--user-data-dir={mkdtemp()}")
-    options.add_argument(f"--data-path={mkdtemp()}")
-    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    # options.add_argument(f"--user-data-dir={mkdtemp()}")
+    # options.add_argument(f"--data-path={mkdtemp()}")
+    # options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome("chromedriver",
+    driver = webdriver.Chrome("/usr/bin/chromedriver",
                               options=options, service_args=["--verbose", "--log-path=/home/valerya/PycharmProjects/qc1.log"])
 
     # driver = webdriver.Chrome( options=options, service_args=["--verbose", "--log-path=/home/valerya/PycharmProjects/qc1.log"])
